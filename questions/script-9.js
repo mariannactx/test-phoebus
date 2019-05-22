6da062356bc5a4f855b99e3f2c8bc2b4bac90d1e
Marianna <mariannactx@gmail.com>
2019-05-22 17:26:00 -0300

    Estrutura base

---
+++ questions/script-9.js
@@ -0,0 +1,10 @@
+/**
+* @prop {string} clientId:
+* @return {number} The check digit.
+*/
+function digitoVerificador(clientId) {
+  // Escreva seu Código aqui
+  return 0;
+}
+
+console.log(digitoVerificador("55555"));
