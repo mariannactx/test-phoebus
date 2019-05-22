d2f6a5dc37d0f8159a0f4b6d8f23846e3b47379c
Marianna <mariannactx@gmail.com>
2019-05-22 20:17:42 -0300

    Solução questão 1

--- questions/script-1.js
+++ questions/script-1.js
@@ -1,1 +1,19 @@
-console.log("1");
+var applyPattern = (first) => {
+
+  var letters = [first];
+  
+  // i - increment, l - letter
+  for(var i = 2, l = 0; i < 6; i++, l++){
+    var charCode = letters[l].charCodeAt(0);
+    letters.push(String.fromCharCode(charCode+i));
+  }
+
+  return letters;
+}
+
+var run = () => {
+  var result = applyPattern('A');
+  byId("tab1").querySelector("samp").innerHTML = JSON.stringify(result);
+}
+
+run();
