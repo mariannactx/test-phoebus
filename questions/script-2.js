ab1106d74f191ff4b432cf52d3b0a1d090c96efe
Marianna <mariannactx@gmail.com>
2019-05-22 19:52:33 -0300

    Solução questão 2

--- questions/script-2.js
+++ questions/script-2.js
@@ -1,1 +1,38 @@
-console.log("2");
+var arrays = {
+  a: [1, 2, 3, 8, 9],
+  b: [6, 5, 4]
+}
+
+var discoverX = () => {
+  var sumA = arrays.a.reduce( (a,b) => a+b )
+  var sumB = arrays.b.reduce( (a,b) => a+b )
+  
+  return {
+    a: 30 - sumA,
+    b: 22 - sumB
+  }
+}
+
+var showSum = (values) => {
+  
+  arrays.a.push(values.a);
+  arrays.b.push(values.b);
+  
+  return {
+    a : {
+      x: values.a,
+      soma: arrays.a.reduce( (a,b) => a+b )
+    },
+    b: {
+      x: values.b,
+      soma: arrays.b.reduce( (a,b) => a+b ),
+    }
+  }
+}
+
+var run = () => {
+  var result = showSum(discoverX());
+  byId("tab2").querySelector("samp").innerHTML = JSON.stringify(result);
+}
+
+run();
